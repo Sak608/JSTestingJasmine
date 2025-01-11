cf7ebc66326355468101593781db8a8140cf2cde
Sakshi Raj <sakshiraj708@gmail.com>
2025-01-11 18:06:34 +0530

    first

---
+++ spec/calculator.spec.js
@@ -0,0 +1,25 @@
+const { add, subtract } = require('../src/calculator')
+
+//test Suite
+// describe('Calculator Testing', function() {
+//     //test case 1
+//     it('Should add two numbers correctly', function() {
+//         expect(add(100,200)).toBe(300);
+//     })
+
+//     //test case 2
+//     it('Should subtract two numbers correctly', function() {
+//         expect(subtract(100,200)).toBe(-100);
+//     })
+
+//     //test case 3
+//     it('Should check matchers', () => {
+//         expect(2+2).toBe(4);
+//         expect([1,2,3]).toContain(2);
+//         expect(null).toBeNull();
+//         expect(true).toBeTruthy();
+//         expect(false).toBeFalsy();
+//     })
+
+// })
+
