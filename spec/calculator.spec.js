390b1aa6fbda231e3c3767db112fff37f721eec0
Sakshi Raj <sakshiraj708@gmail.com>
2025-01-11 18:34:30 +0530

    third

--- spec/calculator.spec.js
+++ spec/calculator.spec.js
@@ -1,3 +1,4 @@
+const Calculator = require("../src/calculator")
 const { add, subtract } = require('../src/calculator')
 
 //test Suite
@@ -23,41 +24,81 @@ const { add, subtract } = require('../src/calculator')
 
 // })
 
-describe('Calculator Testing', function () {
-    describe('Addition Testing', () => {
+// describe('Calculator Testing', function () {
+//     describe('Addition Testing', () => {
 
-        it('Adding two positive numbers', () => {
-            expect(add(100, 200)).toBe(300);
-        })
+//         it('Adding two positive numbers', () => {
+//             expect(add(100, 200)).toBe(300);
+//         })
 
-        it('Adding two negative numbers', () => {
-            expect(add(-100, -200)).toBe(-300);
-        })
+//         it('Adding two negative numbers', () => {
+//             expect(add(-100, -200)).toBe(-300);
+//         })
 
-        it('Adding one positive and one negative numbers', () => {
-            expect(add(100, -200)).toBe(-100);
-        })
+//         it('Adding one positive and one negative numbers', () => {
+//             expect(add(100, -200)).toBe(-100);
+//         })
 
-        it('Adding one negative and one positive numbers', () => {
-            expect(add(-100, 200)).toBe(100);
-        })
+//         it('Adding one negative and one positive numbers', () => {
+//             expect(add(-100, 200)).toBe(100);
+//         })
+//     })
+
+//     describe('Subtraction testing', () => {
+//         it('Subtracting two positive numbers', () => {
+//             expect(subtract(100, 200)).toBe(-100);
+//         })
+
+//         it('Subtracting two negative numbers', () => {
+//             expect(subtract(-100, -200)).toBe(100);
+//         })
+
+//         it('Subtracting one positive and one negative numbers', () => {
+//             expect(subtract(100, -200)).toBe(300);
+//         })
+
+//         it('Subtracting one negative and one positive numbers', () => {
+//             expect(subtract(-100, 200)).toBe(-300);
+//         })
+//     })
+// })
+
+describe('Calculator testing', function () {
+
+    let calculator;
+
+    beforeAll(() => {
+        console.log('Starting Calculator test  suite...')
     })
 
-    describe('Subtraction testing', () => {
-        it('Subtracting two positive numbers', () => {
-            expect(subtract(100, 200)).toBe(-100);
-        })
+    beforeEach(() => {
+        console.log('Before each test..')
+        calculator = new Calculator();
+    })
 
-        it('Subtracting two negative numbers', () => {
-            expect(subtract(-100, -200)).toBe(100);
-        })
+    //test case 1
+    it('Should add two numbers correctly', function() {
+        console.log('testing add method..');
+        calculator.add(100);
+        calculator.add(50);
+        expect(calculator.getResult()).toBe(150);
+    })
 
-        it('Subtracting one positive and one negative numbers', () => {
-            expect(subtract(100, -200)).toBe(300);
-        })
+    //test case 2
+    it('Should subtract two numbers correctly', function() {
+        console.log('testing subtract method..');
+        calculator.add(100);
+        calculator.subtract(50);
+        expect(calculator.getResult()).toBe(50);
+    })
 
-        it('Subtracting one negative and one positive numbers', () => {
-            expect(subtract(-100, 200)).toBe(-300);
-        })
+    //run after each test case
+    afterEach(() => {
+        console.log('after each test case....');
+        calculator.reset();
+    })
+
+    afterAll(() => {
+        console.log('After all calculator test suite...');
     })
 })
--- src/calculator.js
+++ src/calculator.js
@@ -1,10 +1,34 @@
 
-function add(a, b){
-    return a + b;
+// function add(a, b){
+//     return a + b;
+// }
+
+// function subtract(a, b){
+//     return a - b;
+// }
+
+// module.exports = {add, subtract};
+
+class Calculator {
+    constructor() {
+        this.result = 0;
+    }
+
+    add(value) {
+        this.result += value;
+    }
+
+    subtract(value) {
+        this.result -= value;
+    }
+
+    getResult() {
+        return this.result;
+    }
+
+    reset() {
+        this.reset = 0;
+    }
 }
 
-function subtract(a, b){
-    return a - b;
-}
-
-module.exports = {add, subtract};
+module.exports = Calculator;
